a6d27402fe24d617fd9fd84c4ebe12bb88ee62fb
lszejba <lukasz.szejba@gmail.com>
2025-02-14 22:23:25 +0100

    WinForms - ListBox for entries, check items and their notes. Selection works. Notes are shown

--- DailyTasksForm/Form1.Designer.cs
+++ DailyTasksForm/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace DailyTasksForm
+﻿using DailyTasksLibrary;
+
+namespace DailyTasksForm
 {
     partial class Form1
     {
@@ -28,12 +30,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            EntriesListBox = new ListBox();
+            ChecklistItemsListBox = new ListBox();
+            EntryNotesListBox = new ListBox();
+            ChecklistItemNotesListBox = new ListBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // EntriesListBox
+            // 
+            EntriesListBox.FormattingEnabled = true;
+            EntriesListBox.ItemHeight = 15;
+            EntriesListBox.Location = new Point(12, 48);
+            EntriesListBox.Name = "EntriesListBox";
+            EntriesListBox.Size = new Size(377, 169);
+            EntriesListBox.TabIndex = 0;
+            EntriesListBox.SelectedIndexChanged += EntriesListBox_SelectedIndexChanged;
+            // 
+            // ChecklistItemsListBox
+            // 
+            ChecklistItemsListBox.FormattingEnabled = true;
+            ChecklistItemsListBox.ItemHeight = 15;
+            ChecklistItemsListBox.Location = new Point(50, 249);
+            ChecklistItemsListBox.Name = "ChecklistItemsListBox";
+            ChecklistItemsListBox.Size = new Size(339, 184);
+            ChecklistItemsListBox.TabIndex = 1;
+            ChecklistItemsListBox.SelectedIndexChanged += ChecklistItemsListBox_SelectedIndexChanged;
+            // 
+            // EntryNotesListBox
+            // 
+            EntryNotesListBox.FormattingEnabled = true;
+            EntryNotesListBox.ItemHeight = 15;
+            EntryNotesListBox.Location = new Point(434, 48);
+            EntryNotesListBox.Name = "EntryNotesListBox";
+            EntryNotesListBox.Size = new Size(354, 169);
+            EntryNotesListBox.TabIndex = 2;
+            // 
+            // ChecklistItemNotesListBox
+            // 
+            ChecklistItemNotesListBox.FormattingEnabled = true;
+            ChecklistItemNotesListBox.ItemHeight = 15;
+            ChecklistItemNotesListBox.Location = new Point(434, 249);
+            ChecklistItemNotesListBox.Name = "ChecklistItemNotesListBox";
+            ChecklistItemNotesListBox.Size = new Size(354, 184);
+            ChecklistItemNotesListBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 4;
+            label1.Text = ItemsManager.CurrentDate.ToString() + " (" + ItemsManager.CurrentDate.DayOfWeek.ToString() + ")";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(ChecklistItemNotesListBox);
+            Controls.Add(EntryNotesListBox);
+            Controls.Add(ChecklistItemsListBox);
+            Controls.Add(EntriesListBox);
+            Name = "Form1";
+            Text = "Daily Tasks";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox EntriesListBox;
+        private ListBox ChecklistItemsListBox;
+        private ListBox EntryNotesListBox;
+        private ListBox ChecklistItemNotesListBox;
+        private Label label1;
     }
 }
--- DailyTasksForm/Form1.cs
+++ DailyTasksForm/Form1.cs
@@ -1,10 +1,36 @@
-namespace DailyTasksForm
+using DailyTasksLibrary;
+
+namespace DailyTasksForm;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    private ItemsManager manager;
+    public Form1()
     {
-        public Form1()
+        InitializeData();
+        InitializeComponent();
+    }
+
+    private void InitializeData()
+    {
+        manager = new ItemsManager();
+        manager.LoadTasks();
+    }
+
+    private void EntriesListBox_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (EntriesListBox.SelectedItem is Entry selectedEntry)
         {
-            InitializeComponent();
+            ChecklistItemsListBox.DataSource = selectedEntry.Items;
+            EntryNotesListBox.DataSource = selectedEntry.Notes;
+        }
+    }
+
+    private void ChecklistItemsListBox_SelectedIndexChanged(object sender, EventArgs args)
+    {
+        if (ChecklistItemsListBox.SelectedItem is ChecklistItem selectedItem)
+        {
+            ChecklistItemNotesListBox.DataSource = selectedItem.Notes;
         }
     }
 }
