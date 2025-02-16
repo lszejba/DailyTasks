40020a71995ecc62cd016d8e1a23b91381c3c5d9
lszejba <lukasz.szejba@gmail.com>
2025-02-16 13:19:14 +0100

    Entries collection cleanup

--- DailyTasksLibrary/ItemsManager.cs
+++ DailyTasksLibrary/ItemsManager.cs
@@ -9,20 +9,16 @@ namespace DailyTasksLibrary;
 
 public class ItemsManager
 {
-    public List<Entry> entries;
-    public BindingList<Entry> Entries;
+    public BindingList<Entry> Entries { get; private set; }
 
     static DateOnly? currentDate = null;
 
     public ItemsManager()
     {
-        entries = new List<Entry>();
         Entries = new BindingList<Entry>();
-        Entries.AddingNew += new AddingNewEventHandler(Entries_AddingNew);
-        Entries.ListChanged += new ListChangedEventHandler(Entries_ListChanged);
     }
 
-    public int Count { get { return entries.Count; } }
+    public int Count => Entries.Count;
 
     public static DateOnly CurrentDate
     {
@@ -41,29 +37,23 @@ public class ItemsManager
     {
         Console.WriteLine($"[DEBUG] new entry with name: {name}, description: {description} and date: {date}");
         Entry entry = new Entry(date, name, description);
-        entries.Add(entry);
         Entries.Add(entry);
         SaveAll();
     }
 
-    /*public void AddItem(int index, string item)
-    {
-        Entry parent = entries[index];
-        parent.AddItem(item);
-    }*/
-
     public Entry? GetItem(int index)
     {
-        if (entries.Count == 0) return null;
-        if (entries.Count <= index) return null;
-        if (index < 0) return null;
-        return entries[index];
+        if (index < 0 || Count == 0 || Count <= index)
+        {
+            return null;
+        }
+        return Entries[index];
     }
 
     public void PrintEntries(DateOnly date = new DateOnly(), bool showCompletedEntries = false)
     {
         int index = 0;
-        foreach (var item in entries)
+        foreach (var item in Entries)
         {
             Console.WriteLine($"{index++}: {item.FullString()}");
         }
@@ -73,8 +63,7 @@ public class ItemsManager
     {
         var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         const string fileName = "dailytasks.json";
-        // ToDo - update archive (read if needed first), save current state of archive
-        var current = Newtonsoft.Json.JsonConvert.SerializeObject(entries, Newtonsoft.Json.Formatting.Indented);
+        var current = Newtonsoft.Json.JsonConvert.SerializeObject(Entries, Newtonsoft.Json.Formatting.Indented);
         Console.WriteLine(current);
 
         File.WriteAllText(Path.Combine(path, fileName), current);
@@ -96,22 +85,7 @@ public class ItemsManager
 
         if (!string.IsNullOrEmpty(currentStr))
         {
-            entries = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Entry>>(currentStr);
+            Entries = Newtonsoft.Json.JsonConvert.DeserializeObject<BindingList<Entry>>(currentStr);
         }
-
-        foreach(Entry entry in entries)
-        {
-            Entries.Add(entry);
-        }
-    }
-
-    private void Entries_AddingNew(object sender, AddingNewEventArgs e)
-    {
-
-    }
-
-    private void Entries_ListChanged(object sender, ListChangedEventArgs e)
-    {
-
     }
 }
