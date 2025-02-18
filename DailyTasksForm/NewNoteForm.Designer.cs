92a5eba0737eede7d99b181324d264b2b1070bb1
lszejba <lukasz.szejba@gmail.com>
2025-02-18 21:47:51 +0100

    New note form added

---
+++ DailyTasksForm/NewNoteForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace DailyTasksForm
+{
+    partial class NewNoteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ParentNameLabel = new Label();
+            AddNoteButton = new Button();
+            NoteTextBox = new TextBox();
+            SuspendLayout();
+            // 
+            // ParentNameLabel
+            // 
+            ParentNameLabel.AutoSize = true;
+            ParentNameLabel.Location = new Point(9, 9);
+            ParentNameLabel.Name = "ParentNameLabel";
+            ParentNameLabel.Size = new Size(0, 15);
+            ParentNameLabel.TabIndex = 0;
+            // 
+            // AddNoteButton
+            // 
+            AddNoteButton.Enabled = false;
+            AddNoteButton.Location = new Point(428, 27);
+            AddNoteButton.Name = "AddNoteButton";
+            AddNoteButton.Size = new Size(98, 23);
+            AddNoteButton.TabIndex = 1;
+            AddNoteButton.Text = "Add Note";
+            AddNoteButton.UseVisualStyleBackColor = true;
+            AddNoteButton.Click += AddNoteButton_Click;
+            // 
+            // NoteTextBox
+            // 
+            NoteTextBox.Location = new Point(9, 27);
+            NoteTextBox.Name = "NoteTextBox";
+            NoteTextBox.Size = new Size(413, 23);
+            NoteTextBox.TabIndex = 2;
+            NoteTextBox.TextChanged += NodeTextBox_TextChanged;
+            // 
+            // NewNoteForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(539, 70);
+            Controls.Add(NoteTextBox);
+            Controls.Add(AddNoteButton);
+            Controls.Add(ParentNameLabel);
+            Name = "NewNoteForm";
+            Text = "Add new note";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label ParentNameLabel;
+        private Button AddNoteButton;
+        private TextBox NoteTextBox;
+    }
+}
---
+++ DailyTasksForm/NewNoteForm.cs
@@ -0,0 +1,37 @@
+﻿using DailyTasksLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DailyTasksForm
+{
+    public partial class NewNoteForm : Form
+    {
+        ItemsManager _manager;
+        TaskItem _parent;
+        public NewNoteForm(ItemsManager manager, TaskItem parent)
+        {
+            _manager = manager;
+            _parent = parent;
+            InitializeComponent();
+            ParentNameLabel.Text = "Parent: " + _parent.ToString() + "[" + _parent.GetType().Name + "]";
+        }
+
+        public void NodeTextBox_TextChanged(object sender, EventArgs e)
+        {
+            AddNoteButton.Enabled = NoteTextBox.Text.Length > 0;
+        }
+
+        private void AddNoteButton_Click(object sender, EventArgs e)
+        {
+            _manager.AddNote(_parent, NoteTextBox.Text);
+            this.Close();
+        }
+    }
+}
